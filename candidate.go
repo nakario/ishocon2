3620666622e0fb44a7c9d12a04ed4116e68be88a
aokabi <aokabit@gmail.com>
2018-10-07 22:17:20 +0900

    convert int

--- candidate.go
+++ candidate.go
@@ -144,7 +144,7 @@ func getElectionResult() (result []CandidateElectionResult) {
 		r.PoliticalParty = candidate.PoliticalParty
 		r.Sex = candidate.Sex
 		val, _ := VoteCountByCandidateIDMap.Load(candidate.ID)
-		r.VoteCount = val
+		r.VoteCount = int(val)
 		result = append(result, r)
 	}
 	sort.Slice(result, func(i, j int) bool { return result[i].VoteCount > result[j].VoteCount })
