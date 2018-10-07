82013b2445da1980b235dd7bd69902d610bfd184
isucon <isucon@example.com>
2018-10-07 09:57:36 +0000

    fix

--- candidate.go
+++ candidate.go
@@ -1,9 +1,10 @@
 package main
 
 import (
-	"html/template"
 	"errors"
+	"html/template"
 )
+
 // Candidate Model
 type Candidate struct {
 	ID             int
@@ -65,70 +66,73 @@ var candidates = []Candidate{
 	Candidate{30, "伊藤 五郎", "国民元気党", "男"},
 }
 
-var VoteCountByCandidateIDMap map[int]int{
-	1:0,
-	2:0,
-	3:0,
-	4:0,
-	5:0,
-	6:0,
-	7:0,
-	8:0,
-	9:0,
-	10:0,
-	11:0,
-	12:0,
-	13:0,
-	14:0,
-	15:0,
-	16:0,
-	17:0,
-	18:0,
-	19:0,
-	20:0,
-	21:0,
-	22:0,
-	23:0,
-	24:0,
-	25:0,
-	26:0,
-	27:0,
-	28:0,
-	29:0,
-	30:0,
+var VoteCountByCandidateIDMap = map[int]int{
+	1:  0,
+	2:  0,
+	3:  0,
+	4:  0,
+	5:  0,
+	6:  0,
+	7:  0,
+	8:  0,
+	9:  0,
+	10: 0,
+	11: 0,
+	12: 0,
+	13: 0,
+	14: 0,
+	15: 0,
+	16: 0,
+	17: 0,
+	18: 0,
+	19: 0,
+	20: 0,
+	21: 0,
+	22: 0,
+	23: 0,
+	24: 0,
+	25: 0,
+	26: 0,
+	27: 0,
+	28: 0,
+	29: 0,
+	30: 0,
 }
-
 
 func getInitAllCandidatesDOM() template.HTML {
 	result := ""
-	for _,candidate := range candidates {
+	for _, candidate := range candidates {
 		result += `<option value="` + candidate.Name + `">` + candidate.Name + `</option>`
 	}
 	return template.HTML(result)
 }
+
 var getAllCandidatesDOM = getInitAllCandidatesDOM()
+
 func getCandidate(candidateID int) (c Candidate, err error) {
-	if (candidateID  <= 0 || candidateID >= 30) {
+	if candidateID <= 0 || candidateID >= 30 {
 		err = errors.New("yee")
 	}
-	c = candidates[candidateID - 1]
+	c = candidates[candidateID-1]
 	err = nil
 	return
 }
 
 func initCandidateByNameMap() map[string]Candidate {
 	result := make(map[string]Candidate)
-	for _,candidate := range candidates {
+	for _, candidate := range candidates {
 		result[candidate.Name] = candidate
 	}
 	return result
 }
+
 var candidadeByNameMap = initCandidateByNameMap()
+
 func getCandidateByName(name string) (c Candidate, err error) {
 	c, ok := candidadeByNameMap[name]
 	if !ok {
 		err = errors.New("no candidate")
-	}else {
+	} else {
 		err = nil
 	}
 	return
